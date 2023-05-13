fbf0badf289fcee31db5020131ad29de92865bc2
amtenuJava <java.amanuel@gmail.com>
2023-05-13 05:30:05 -0600

    feat: Add new feature
    This commit adds a new feature that creates a record and create a linkedList containg the record. It also creates a method to add a new city after checking for duplicates both as a whole or duplicate name (Ignore case).

--- src/dev/LinkedListChallenge.java
+++ src/dev/LinkedListChallenge.java
@@ -9,7 +9,65 @@ package dev;
  -  created a linkedList , ordered by the distance from Asmara
  -  No duplicated allowed
 
+     Asmara <-> Mendefera   = 51kms
+            <-> Dekemhare   = 40kms
+            <-> Keren   = 93kms
+            <-> Massawa   = 112kms
+            <-> Adi Tekelezan   = 40kms
+            <-> Duba Ruba   = 30kms
+            <-> Tesseney   = 359kms
+            <-> Senafe   = 132kms
+            <-> Assab   = 1065kms
+            <-> Nakfa   = 249kms
+
+ - create menu options
+
+  Available actions (select word or letter):
+    (F)orward
+    (B)ackwards
+    (L)ist Places
+    (M)enu
+    (Q)uit
+
  */
+
+import java.util.LinkedList;
+
+record Place(String name, int distance) {
+    @Override
+    public String toString() {
+        return String.format("%s (%d)", name, distance);
+    }
+}
+
+
 public class LinkedListChallenge {
+
+    public static void main(String[] args) {
+        LinkedList<Place> citiesToVisit = new LinkedList<>();
+        Place keren = new Place("Keren", 93);
+        addCity(citiesToVisit, keren);
+        System.out.println(citiesToVisit);
+
+
+    }
+
+    private static void addCity(LinkedList<Place> list, Place place) {
+        if (list.contains(place)) {
+            System.out.println("Duplicate -> " + place);
+            return;
+        }
+
+        for(Place pl:list){
+            if(pl.name().equalsIgnoreCase(place.name())){
+                System.out.println("Duplicate-> "+ place);
+            }
+        }
+
+
+        list.add(place);
+
+    }
+
 
 }
