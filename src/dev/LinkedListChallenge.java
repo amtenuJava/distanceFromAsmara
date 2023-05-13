22cfd6250cf5149c784bfb39125ccc22f8742613
amtenuJava <java.amanuel@gmail.com>
2023-05-13 04:46:41 -0600

    Initial commit with project description.

---
+++ src/dev/LinkedListChallenge.java
@@ -0,0 +1,15 @@
+package dev;
+/*
+
+       Distance of Cities Ordered by distance from Asmara,Eritrea
+       __________________________________________________________
+
+ -  Used ListIterator to move forward and backward
+ -  need to create type that has a town and distance
+ -  created a linkedList , ordered by the distance from Asmara
+ -  No duplicated allowed
+
+ */
+public class LinkedListChallenge {
+
+}
